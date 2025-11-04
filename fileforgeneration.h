6e44eedac530318f01161b1b1abf129d57926abe
LoFiSwanL <daiki.ren710@gmai.com>
2025-11-04 19:12:19 +0200

    first and last comit

--- fileforgeneration.h
+++ fileforgeneration.h
@@ -4,20 +4,22 @@
 #include <fstream>
 #include <vector>
 #include <random>
+#include <tuple>
 
-struct operation_frequency{
-    std::string command;
-    int index;
-    double freq;
-};
+using Comand_set = std::vector<std::tuple<std::string, int, double>>;
 
 class generator{
-    const std::vector<operation_frequency> var9_freq = {{"read", 0, 0.1}, {"read", 0, 0.1}, {"write", 0, 0.1}, {"read", 1, 0.1}, {"write", 1, 0.1}, {"read", 2, 0.4}, {"write", 2, 0.05}, {"string", -1, 0.15}};
+    const Comand_set var9_freq = {{"read", 0, 0.1}, {"write", 0, 0.1}, {"read", 1, 0.1}, {"write", 1, 0.1}, {"read", 2, 0.4}, {"write", 2, 0.05}, {"string",   -1, 0.15}};
 
-    void generate(const std::string &fileName, const std::vector<operation_frequency> &freq, long long num_operations);
+    void generate_file(const std::string &file, const Comand_set &freq, long long num_operations);
 
-    std::vector<std::string> generate_comands(const std::vector<operation_frequency> &freq, long long num_operations);
+    std::vector<std::string> generate_comands(const Comand_set &freq, long long num_operations);
 
 public:
-    
+    static constexpr long long DEFAULT_OPS_COUNT = 10000;
+
+    void generate_all_files(long long num_operations = DEFAULT_OPS_COUNT);
+
+    Comand_set get_equal_freq();
+    Comand_set get_bad_freq();
 };
